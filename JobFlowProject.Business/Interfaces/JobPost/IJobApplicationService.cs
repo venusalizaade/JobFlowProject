6daec6ad7099f191d66cc61c93f845b4051560a0
VenusA <venusbeygi@gmail.com>
2026-07-17 06:24:45 +0330

    add Interface service for jopapplication class

---
+++ JobFlowProject.Business/Interfaces/JobPost/IJobApplicationService.cs
@@ -0,0 +1,17 @@
+﻿using JobFlowProject.Business.Dto.Commands;
+using JobFlowProject.Business.Dto.CompanyDto;
+using JobFlowProject.Domain.Enums;
+
+namespace JobFlowProject.Business.Interfaces.JobPost;
+
+public interface IJobApplicationService
+{
+    Task ApplyAsync(Guid requesterId, ApplyJobCommand command);
+
+
+    Task<List<JobApplicationResponseDto>>
+        GetJobApplicationsAsync(Guid requesterId, Guid jobPostId);
+
+
+    Task ChangeStatusAsync(Guid requesterId, ChangeApplicationStatusCommand command);
+}
---
+++ JobFlowProject.Domain/Entities/Location/Province.cs
@@ -0,0 +1,27 @@
+﻿namespace JobFlowProject.Domain.Entities;
+
+public class Province : BaseEntity
+{
+    private Province() { }
+
+    public Province(string name)
+    {
+        Name = name;
+    }
+
+    /// <summary>
+    /// نام استان
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// شهرهای استان
+    /// </summary>
+    public ICollection<City> Cities { get; private set; } = new List<City>();
+
+    public override void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+            throw new Exception("Province name cannot be empty.");
+    }
+}
